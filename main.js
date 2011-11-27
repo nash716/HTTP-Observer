ec815cac17c345db91c4a18730a114c1347cc55c
nash <contact@nash-dev.com>
2011-11-27 11:16:59 +0900

    最低限の機能が完成

---
+++ main.js
@@ -0,0 +1,79 @@
+const statusIcon = '<span class="sent">●</span>';
+const request = '<b><span class="r">Request Headers</span></b>: <br />';
+const response = '<b><span class="r">Response Headers</span></b>: <br />';
+const statusline = '<b><span class="r">Status Line</span></b>: <br />';
+
+const ext_version = 0.1;
+var isCapture = true;
+
+$(document).ready(function() {
+	if (ext_version < version) {
+		$('div#header hr').before(' | New version available!');
+	}
+});
+
+chrome.experimental.webRequest.onSendHeaders.addListener(onSendHeaders, { }, ['requestHeaders']);
+
+chrome.experimental.webRequest.onCompleted.addListener(function(details) {
+	if (isCapture && !$('div#' + details.requestId)) {
+		return;
+	}
+	if (isCapture) {
+		$('div#' + details.requestId).children('span.sent')
+							.removeClass('sent')
+							.addClass('ok'); // TODO
+		
+		var str = $('div#' + details.requestId).next().html();
+		str = statusline + details.statusLine + '<br />' + str;
+		str += response;
+		
+		for (var key in details.responseHeaders) {
+			str += '<b>' + text(details.responseHeaders[key].name) + '</b>: ' + text(details.responseHeaders[key].value) + '<br />';
+		}
+		
+		$('div#' + details.requestId).next().html(str);
+	}
+}, { }, ['statusLine', 'responseHeaders']);
+
+chrome.experimental.webRequest.onErrorOccurred.addListener(function(details) {
+	if (isCapture && !$('div#' + details.requestId)) {
+		return;
+	}
+	if (isCapture) {
+		$('div#' + details.requestId).children('span.sent')
+							.removeClass('sent')
+							.addClass('error');
+		var str = $('div#' + details.requestId).next().html();
+		str += '<br /><b><span class="error">Error:</span></b><br />' + details.error;
+		$('div#' + details.requestId).next().html(str);
+	}
+}, { });
+
+function text(str) {
+	str = str.split('&').join('&amp;'); //.replace('&', '&amp;');
+	str = str.split('"').join('&quot;'); //.replace('"', '&quot;');
+	str = str.split('<').join('&lt;'); //.replace('<', '&lt;');
+	str = str.split('>').join('&gt;'); //.replace('>', '&gt;');
+	str = str.split("'").join('&#039;'); //.replace("'", '&#039;');
+	return str;
+}
+
+function onSendHeaders(details) {
+	if (isCapture) {
+		var str = '';
+		for (var key in details.requestHeaders) {
+			str += '<b>' + text(details.requestHeaders[key].name) + '</b>: ' + text(details.requestHeaders[key].value) + '<br />';
+		}
+		$('<div>').html(statusIcon + text(details.url))
+				.addClass('caption')
+				.attr('id', details.requestId)
+				.click(function() {
+					$(this).next().toggle();
+				}).appendTo($('div#content'));//$(document.body));
+				
+		$('<div>').html(request + str)
+				.addClass('detail')
+				.css('display', 'none')
+				.appendTo($('div#content'));//$(document.body));
+	}
+}
